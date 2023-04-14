9600b8f0d8987f461270e2ea89d24b08ecc2b8e4
Cao Hoàng Kha <caohoangkha99@gmail.com>
2023-04-14 17:22:15 +0700

    rr

--- api/app/Http/Controllers/DichVu.php
+++ api/app/Http/Controllers/DichVu.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Dichvu as MDichvu;
+use App\Models\KhDv as MKhDv;
+use App\Models\KhachHang as MKhachHang;
+use Illuminate\Support\Facades\DB;
 class DichVu extends Controller
 {
-    //
+    public function geDanhSachDichVu(){
+        $dichvus = MDichvu::all();
+            return response()->json([
+                'status'=>200,
+                'data'=>$dichvus
+            ]);
+    }
+    public function getDSDichVuByIdKhachHang($sdt){
+        $khachhang = MKhachHang::where('KH_SDT', $sdt)->first();
+        $dichvu =  DB::table('kh_dv')->select("kh_dv.DV_ID","kh_dv.KH_ID","dichvu.DV_TENDV")
+        ->leftJoin('dichvu', 'kh_dv.DV_ID', '=', 'dichvu.DV_ID')
+        ->where('kh_dv.KH_ID', $khachhang->KH_ID)
+        ->get();
+        return response()->json([
+            'status'=>200,
+            'data'=>$dichvu
+        ]);
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhieuBaoHong;
+use App\Http\Controllers\KhachHang;
+use App\Http\Controllers\DichVu;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('DichVu/geDanhSachDichVu', [DichVu::class,'geDanhSachDichVu',]);
+Route::get('KhachHang/getDSBaoHongBySdtKhachHang/{sdt}', [KhachHang::class,'getDSBaoHongBySdtKhachHang',]);
+Route::get('PhieuBaoHong/danhsachphieu', [PhieuBaoHong::class,'getDSPhieuBaoHong',]);
+Route::POST('PhieuBaoHong/insert', [PhieuBaoHong::class,'insert',]);
+Route::POST('PhieuBaoHong/update', [PhieuBaoHong::class,'update',]);
+Route::POST('PhieuBaoHong/xacNhan', [PhieuBaoHong::class,'xacNhan',]);
+Route::POST('PhieuBaoHong/banGiaoXuLy', [PhieuBaoHong::class,'banGiaoXuLy',]);
+Route::POST('PhieuBaoHong/hoanThanh', [PhieuBaoHong::class,'hoanThanh',]);
+Route::get('DichVu/getDSDichVuByIdKhachHang/{sdt}', [DichVu::class,'getDSDichVuByIdKhachHang',]);
+
+
