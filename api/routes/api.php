6d1fb94a2c4d9cfa7e3341c27897dc421ccb2104
Cao Hoàng Kha <caohoangkha99@gmail.com>
2023-04-15 11:49:50 +0700

    Bổ sung api getDichVuBySdtKhachHang

--- api/routes/api.php
+++ api/routes/api.php
@@ -31,6 +31,8 @@ Route::POST('PhieuBaoHong/xacNhan', [PhieuBaoHong::class,'xacNhan',]);
 Route::POST('PhieuBaoHong/banGiaoXuLy', [PhieuBaoHong::class,'banGiaoXuLy',]);
 Route::POST('PhieuBaoHong/hoanThanh', [PhieuBaoHong::class,'hoanThanh',]);
 Route::POST('PhieuBaoHong/danhGia', [PhieuBaoHong::class,'danhGia',]);
+Route::get('DichVu/getDSDichVuByIdKhachHang/{sdt}', [DichVu::class,'getDSDichVuByIdKhachHang',]);
 Route::get('NhanVien/getDSPhieuBaoHongBySdtNhanVien/{sdt}', [NhanVien::class,'getDSPhieuBaoHongBySdtNhanVien',]);
+
 
 
